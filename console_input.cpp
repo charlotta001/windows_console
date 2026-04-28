127e831c32d30b1297d102d4517fbf053d1deb46
Hafizh Nurfitrian Syah <hafizhvtuber@gmail.com>
2026-04-28 09:26:13 +0700

    bisa (enter | backspace)

--- console_input.cpp
+++ console_input.cpp
@@ -6,28 +6,33 @@ int main() {
     
     HANDLE hConsole, hConsoleInput, hConsoleOutput;
     wchar_t buffer[255];
-    INPUT_RECORD input_record[4000];
+    INPUT_RECORD input_record[100];
+    INPUT_RECORD input_mouse[10];
     DWORD eventRead;
 
     wchar_t *teks = L" aku suka matcha" ;
     int len = wcslen(teks);
 
-    int row = 0;
-    int cursorCol = 0;
+    short row = 0;
+    short cursorCol = 0;
     int cursorColBef = 0;
     int colHistory[50] = {};
     // CHAR_INFO (ARRAY OF STRUCT)
     CHAR_INFO charInfo[80 * 50] = {};
     COORD topLeft = {0,0};
     COORD sizeConsole = {80,50};
+
+
+    // mouse event
+    COORD mousePos = {0,0};
     
    
 
 
-    // for(int i = 0; i < len; i++){
-    //     charInfo[i].Char.UnicodeChar = teks[i];
-    //     charInfo[i].Attributes = FOREGROUND_BLUE | BACKGROUND_GREEN;
-    // }
+    for(int i = 0; i < len; i++){
+        charInfo[i].Char.UnicodeChar = teks[i];
+        charInfo[i].Attributes = FOREGROUND_BLUE | BACKGROUND_GREEN;
+    }
 
     hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE,
                                         FILE_SHARE_READ | FILE_SHARE_WRITE,
@@ -39,6 +44,7 @@ int main() {
     hConsoleInput = GetStdHandle(STD_INPUT_HANDLE);
     hConsoleOutput = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleActiveScreenBuffer(hConsole);
+    SetConsoleMode(hConsole, ENABLE_MOUSE_INPUT | ENABLE_EXTENDED_FLAGS);
 
 
 
@@ -56,20 +62,37 @@ int main() {
                 SetConsoleCursorPosition(hConsole, cursorPos);
 
                 KEY_EVENT_RECORD key = input_record[i].Event.KeyEvent;
+                MOUSE_EVENT_RECORD mouse = input_record[i].Event.MouseEvent;
 
+
+                if(mouse.dwControlKeyState & CAPSLOCK_ON){
+                    // short x = mouse.dwMousePosition.X;
+                    // short y = mouse.dwMousePosition.Y;
+                    // mousePos = {x, y};
+                    mousePos= mouse.dwMousePosition;
+                }
+
+                if(mouse.dwButtonState & FROM_LEFT_1ST_BUTTON_PRESSED){
+                    cursorPos = mousePos;
+                }
 
                 if(key.bKeyDown){
+
+                    // HANDLE ESCAPE
                     if(key.wVirtualKeyCode == VK_ESCAPE){
                         running = true;
                     }
 
+
+                    // HANDLE ENTER
                     if(key.wVirtualKeyCode == VK_RETURN){
                         colHistory[row] = cursorCol;
                         row++;
                         cursorCol = 0;
                     }
                 
-                    // HANDLE KEYBOARD INPUT
+                    // HANDLE KEYBOARD INPUTasdf
+                
                     if(key.uChar.UnicodeChar >= 32){
                         int idx = row * 80 + cursorCol;
                         charInfo[idx].Char.UnicodeChar = key.uChar.UnicodeChar;
@@ -77,6 +100,8 @@ int main() {
                         cursorCol++;
                     }  
                     
+
+                    // HANDLE TAB
                     if(key.wVirtualKeyCode == VK_TAB){
                         int totalCol = cursorCol + 4;
                         for(int i = 0; i < totalCol; i++){
@@ -85,19 +110,23 @@ int main() {
                       
                     }
 
-
                     // HANDLE BACKSPACE
                     if(key.wVirtualKeyCode == VK_BACK){
 
                     if(row >= 1 && cursorCol == 0){
+                        
                         row--;
                         cursorCol = colHistory[row];
+
+                        int idx = row * 80 + cursorCol;
+                        charInfo[cursorCol].Char.UnicodeChar = ' ';
+                        
+                         
                     } else if(cursorCol >= 1){
-              
-                        int idx = row * 80 + cursorCol--;
+                        cursorCol--;
+                        int idx = row * 80 + cursorCol;
                         charInfo[idx].Char.UnicodeChar= ' ';
                         charInfo[idx].Attributes = FOREGROUND_RED;
-
                     }
                 }
 
